30f200d7e0d610c5350cd4d08761d0e219520e28
Brodzman <thbrodz@gmail.com>
2025-01-20 13:20:31 +1100

    Init a window and add .gitignore

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill(000000)
+        pygame.display.flip()
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
